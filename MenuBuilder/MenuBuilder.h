d7c2bf6f5d6d209a711575f438114d29e855ac84
Zhu Shengqi <shengqi@teambition.com>
2017-05-26 16:49:29 +0800

    Initial Commit

---
+++ MenuBuilder/MenuBuilder.h
@@ -0,0 +1,19 @@
+//
+//  MenuBuilder.h
+//  MenuBuilder
+//
+//  Created by Zhu Shengqi on 26/05/2017.
+//  Copyright © 2017 zetasq. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for MenuBuilder.
+FOUNDATION_EXPORT double MenuBuilderVersionNumber;
+
+//! Project version string for MenuBuilder.
+FOUNDATION_EXPORT const unsigned char MenuBuilderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MenuBuilder/PublicHeader.h>
+
+
